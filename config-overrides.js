03db3ca5ef37bc3ab097b38355de3c8f957be672
손동천 <kij8025@naver.com>
2026-02-14 08:28:27 +0900

    kkkk

--- config-overrides.js
+++ config-overrides.js
@@ -5,5 +5,11 @@ module.exports = function override(config) {
     ...config.resolve.alias,
     "@": path.resolve(__dirname, "src"),
   };
+
+  // WebSocket 미사용: HMR용 wss://.../ws 연결 시도 제거 (연결 실패 경고 방지)
+  if (config.devServer) {
+    config.devServer.webSocketServer = false;
+  }
+
   return config;
 };
